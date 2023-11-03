1b8d82ca3992b71777b0511f9efffe4bffd15958
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-11-02 18:27:59 -0700

    average method

--- src/winter2023Assignments/week07.java
+++ src/winter2023Assignments/week07.java
@@ -24,66 +24,64 @@ public class week07 {
 	//giving the number of students entered an array
 		int students = scanner.nextInt(); // read an integer from the user
 
+		
+		
+		
 	while(students < 1) { 
-		System.out.println("Invalid try again \n");
+		System.out.println("Invalid input. Please enter a valid number of students: ");
+        students = scanner.nextInt();
+		
+		
 	}
-	else if (students == 1){ 
+	if (students == 1){ 
 		System.out.println("Enter the name of the student followed by test score\n");
 	}
 
-	else if{ 
+	else { 
 		System.out.println("Enter the amount of name of the " + students + " students followed by test score \n");
 	}
 	
 	
 	
 	
-	
+	/*
 	//////////////////////////////////////////////////////////////////
 	//			Declaration of varriables
 	//////////////////////////////////////////////////////////////////
-	//Scanner StudentScanner = new scanner;
-	//String[] studentNames = new String[students];	
-	//studentNames[i] = scanner.next(); 
+*/
+	
+	
+	
 	String[] studentNames = new String[students];
 	
 	
-	
+	/*
 	// we need to read both but individually 
-	
+	*/
 	
 	
 	double [] testScores = new double[students];
-	//Scanner StudentsScores = new scanner;
-	//int[] testScores = new int[students];
-	//testScores[i] = scanner.nextInt(); 
+
 	
 	
 	
-	
-	
-	
+	/*
 	
 	///     giving each student a name and test score
-	//Scanner scanner = new Scanner(System.in); 
-	//students[] = Scanner.new;
-	//students[] = scanner.next();
-	
-	
+*/
+/*	
 	/// reading the input for the students
-	
+*/	
 	
 
-	//////////////////////////////////////////////////////////////////
+/*	//////////////////////////////////////////////////////////////////
 	//			looping for name and test score to store in variable
 	//////////////////////////////////////////////////////////////////
-	
+*/	
 	
 	
 	
 	for (int i = 0 ; i < students; i++ ) { 
-	//	System.out.println(studentNames +" " + testScores);
-	//	System.out.println(studentNames[i] + " " + testScores[i]);
 	System.out.println("Enter the name of student "+ (i +1) + " : ");
 	studentNames[i] = scanner.next(); 
 	
@@ -92,57 +90,50 @@ public class week07 {
 	
 	}
 	
-	
-	
+	System.out.println("\n");
+	System.out.println("*** here is what I have for your data ***");
 	
 
-
+/*
 	//////////////////////////////////////////////////////////////////
 	//			loops the name of the name and test score to output
 	//////////////////////////////////////////////////////////////////
-	
+*/	
 	
 	for (int i = 0; i < students; i++) { 
-//	System.out.printf(".2f",studentNames[i] + "  " + ".2f",testScores[i]);
-	//System.out.printf("%s  %.2f", studentNames[i], testScores[i]);
+//System.out.println("*** Sorted data ***\n");
 		System.out.printf("%s  %.2f\n", studentNames[i], testScores[i]);
 
 	}
-
-scanner.close();
-	
+    double averageScore = calculateAverageScore(testScores);
+//scanner.close();
+	System.out.println("average is : " +averageScore);
+	scanner.close();
 	}
 
-
+/*
 // A method that calculates and returns the average of the 
 //test scores. The method will need the test score array.
+*/
 
 
 
+public static double calculateAverageScore(double[] testScores) { 
 
-//public void average() {
-public double calculateAverageScore(double[] testScores) { // scorePiles are the test scores in total 
-	// adds a number to to the sumtotal
-	double cardsUltimatelyInDeck = 0;
-	int numStudents = testScores.length;
-	for (int i = 0; i < testScores.length; i++ ) { 
+	double sum = 0;
 
+	for(double score : testScores) { 
+		sum += score;
 
-	//sumTotal =+ sumTotal / sumTotal.length;
-		cardsUltimatelyInDeck += testScores[i];
 	}
 	
 	
 	
-	double average = cardsUltimatelyInDeck / testScores.length;   // calculating the average
+	double average = sum / testScores.length;   // calculating the average
 
-	// the .length priortie is to count the size of number of element in array its variable is .
 	
-	//int count = 0;
-
-System.out.println("average is : " +average);
-return average;
-}
+	return average;
+	}
 
 }
 
