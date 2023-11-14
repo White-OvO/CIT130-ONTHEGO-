f1db4139056d410c730ea3752a8b2f9472cb9d05
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-11-14 11:19:24 -0800

    initializing no args method

---
+++ src/week09HW/Pool.java
@@ -0,0 +1,27 @@
+package week09HW;
+
+abstract class Pool {
+	// Start with defining the utility class first. Test 
+	//your code incrementally in the test class. Create an 
+	//object, use its methods, test it, before moving on to
+	//de;fine more objects and test more cases.
+
+	
+	
+	// Class Pool:
+	//	The class should have private instance fields to store the color of a pool, 
+	//length (in feet), width (in feet), depth (in feet), area (in square feet) and
+	//the volume (in cubic feet) of the pool. Also provide the following methods:
+private String color; 
+private double length;
+private double width;
+private double depth; 
+private double area; 
+private double volume;
+
+public Pool(String color, double length, double width, double depth  ) {
+
+//void label() { 
+//	System.out.println("Pool data: ");
+}
+}
---
+++ src/week09HW/constructor.java
@@ -0,0 +1,5 @@
+package week09HW;
+
+public class constructor {
+
+}
